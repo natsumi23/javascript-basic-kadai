2e849dcbbaf6c4b93cbe2c8176664ca05c775428
natsumi23 <ailes.fleur@gmail.com>
2024-07-10 10:52:20 +0900

    kadai_mdn_jsのフォルダをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,14 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+console.log(today);
+
+const todaysDate = () => {
+  console.log(year);
+  console.log(month);
+  console.log(day);
+}
+
+console.log(year + '年' + month + '月' + day + '日')
